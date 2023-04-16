2c28398d5a09e23ea5bca3873b398c9aa2629dac
Sipko <Sipkovandam@gmail.com>
2023-04-16 14:51:39 +0200

    Reverting change
    Paste was not what was making it slow in my case

--- R/input-selectpicker.R
+++ R/input-selectpicker.R
@@ -336,7 +336,7 @@ selectOptions <- function(choices,
   }
   
   # paste everything together
-  htmltools::HTML(captureOutput(html, collapse = "\n"))
+  htmltools::HTML(paste(html, collapse = "\n"))
 }
 
 
